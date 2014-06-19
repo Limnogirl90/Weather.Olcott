81044ebd247f5fbf6fbed3c2fd46f725c5f1bba1
Kingdon Barrett <yebyen@gmail.com>
2014-06-18 22:04:27 -0400

    aaaaghghhh

--- lib/business.rb
+++ lib/business.rb
@@ -12,46 +12,49 @@ class Business
 
 private
 
-  def log_in
+  attr_accessor :go_button, :timeframe, :date_selector, :font_node, :str, :arr, :clown, :s
+
+  def do_setup
+
     visit LOGIN_URI
-
-    s = nil
     reports_form = find(:xpath, '//form[@name="getData"]')
-    w = window_opened_by do
-      within reports_form do
-        go_button = find(:xpath, '//td[@class="a8"][4]/a')
-        timeframe = find(:xpath, '//td[@class="a8"][3]')
-        within timeframe do
-          find(:xpath, 'input[@value="no"]').click
-          date_selector = find(:xpath, 'font/select')
-          date_selector.select('April 22nd, 2014') #'20140423')
-          within date_selector do
-            s = all(:xpath, 'option')
 
-            #GAH: won't work
-            #my_option = find(:xpath, 'option[@value="20140423"]')
-            #select my_option.value
-          end
-        end
-        go_button.click
-        #s = find(:xpath, '(//td[@class="a8"]/font/select[@class="a8"])/option').all
-      end
+    # w was not useful, had to call cliPopup by name to within_window
+    #w = window_opened_by do
+    #end
+    within reports_form do
+      go_button = find(:xpath, '//td[@class="a8"][4]/a')
+      timeframe = find(:xpath, '//td[@class="a8"][3]')
+      #s = find(:xpath, '(//td[@class="a8"]/font/select[@class="a8"])/option').all
     end
-
-    s.each do |opt|
-      puts "#{opt.value}: #{opt.text}"
-      #puts "#{opt.methods}"
-      #break
-    end
-    
-    within_window 'cliPopup' do
-      font_node = find(:xpath, '//pre/font')
-      #print font_node.text
-      str = font_node.text
-      arr = str.split("...................................")
-      clown = arr[2].clone
-      ap clown.scan(/[+\s[:alpha:]()]+[\d\s\.-]+/).map(&:strip)
+    within timeframe do
+      find(:xpath, 'input[@value="no"]').click
+      date_selector = find(:xpath, 'font/select')
     end
   end
 
+  def log_in
+    do_setup
+    within date_selector do
+      s = all(:xpath, 'option')
+    end
+
+    s.each do |opt|
+      #puts "#{opt.methods}"
+      #break
+      puts "#{opt.value}: #{opt.text}"
+      date_selector.select(opt.text) #'April 22nd, 2014') #'20140423')
+      go_button.click
+
+      within_window 'cliPopup' do
+        font_node = find(:xpath, '//pre/font')
+        #print font_node.text
+        str = font_node.text
+        arr = str.split("...................................")
+        clown = arr[2].clone
+        p clown.scan(/[+\s[:alpha:]()]+[\d\s\.-]+/).map(&:strip)
+        do_setup
+      end
+    end
+  end
 end
