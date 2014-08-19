86600e2d85963381b1e1d6af76b41a3dbde635ed
Kingdon Barrett <yebyen@gmail.com>
2014-08-18 22:33:44 -0400

    whitespace

--- data/data_3.sh
+++ data/data_3.sh
@@ -1,1 +1,7 @@
-egrep '\[ [0-6]\]' 2014*.out|grep -v 'MONTH TO DATE'|egrep '(MINIMUM|MAXIMUM|AVERAGE) (-)?([0-9])+'|egrep -o '^([0-9])+\.out:    \[ [0-6]\] ".*(MINIMUM|MAXIMUM|AVERAGE) (-)?([0-9])+'|sed -e 's/\.out:    \[ [0-9]\]//g'|sed -e 's/TEMPERATURE (F) YESTERDAY //g'|sed -e s/\"//g
+                            egrep '\[ [0-6]\]' 2014*.out  |\
+        grep -v 'MONTH TO DATE'|\
+   egrep '(MINIMUM|MAXIMUM|AVERAGE) (-)?([0-9])+'|\
+egrep -o '^([0-9])+\.out:    \[ [0-6]\] ".*(MINIMUM|MAXIMUM|AVERAGE) (-)?([0-9])+'|\
+                           sed -e 's/\.out:    \[ [0-9]\]//g'|\
+                         sed -e 's/TEMPERATURE (F) YESTERDAY //g'|\
+                     sed -e s/\"//g
